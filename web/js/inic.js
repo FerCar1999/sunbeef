0924c896c2e225dcad2cce7024183556dedc053f
Fernando Carranza <foreverfas_fc@hotmail.com>
2020-07-03 16:56:03 -0600

    Ultimos cambios

--- web/js/inic.js
+++ web/js/inic.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
     $('.sidenav').sidenav();
+
+    $('.slider').slider();
 });
 
 function empresa() {
     $('html,body').animate({
         scrollTop: $("#empresa").offset().top - 60
     }, 'slow');
-    //$('.sidenav').sidenav('close');
+    $('.sidenav').sidenav('close');
 }
 
 function equipo() {
     $('html,body').animate({
         scrollTop: $("#equipo").offset().top - 60
     }, 'slow');
-    //$('.sidenav').sidenav('close');
+    $('.sidenav').sidenav('close');
 }
 
 function producto() {
     $('html,body').animate({
         scrollTop: $("#producto").offset().top - 60
     }, 'slow');
-    //$('.sidenav').sidenav('close');
+    $('.sidenav').sidenav('close');
 }
 
 function contacto() {
     $('html,body').animate({
         scrollTop: $("#contacto").offset().top - 60
     }, 'slow');
-    //$('.sidenav').sidenav('close');
+    $('.sidenav').sidenav('close');
 }
